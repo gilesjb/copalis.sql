77665565241ac06822df765b8b2270806e01d34c
Giles Burgess <gilesjb@gmail.com>
2012-03-07 00:10:56 -0800

    Moved tests into correct packages

--- src/test/java/org/copalis/sql/common/ParameterizedStatementTest.java
+++ src/test/java/org/copalis/sql/common/ParameterizedStatementTest.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.copalis.sql;
+package org.copalis.sql.common;
 
 import java.sql.SQLException;
 import java.util.Arrays;
--- src/test/java/org/copalis/sql/results/ResultsPropertyTest.java
+++ src/test/java/org/copalis/sql/results/ResultsPropertyTest.java
@@ -13,10 +13,12 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.copalis.sql;
+package org.copalis.sql.results;
 
 import junit.framework.TestCase;
 
+import org.copalis.sql.MockResults;
+import org.copalis.sql.Results;
 import org.copalis.sql.results.ResultsProperty;
 
 /**
